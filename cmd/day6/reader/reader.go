f3b1d48d4d3ef839e9037f461f777f36981ee65f
Conor McErlean <development@cmcerlean.co.uk>
2023-12-07 15:34:31 +0000

    day 6 part 2, bringing back the GCSE maths

--- cmd/day6/reader/reader.go
+++ cmd/day6/reader/reader.go
@@ -8,14 +8,10 @@ import (
 )
 
 func ReadFile() map [int]int {
-	file := common.ReadFileFromArgs()
-	time := strings.Split(file[0], ":")[1]
-	distance := strings.Split(file[1], ":")[1]
+	times, distances := ReadAndBreakUpIntoComponents() 
+
 	timeToDistance := make(map[int]int)
-	times := strings.Split(time, "  ")
-	distances := strings.Split(distance, "  ")
-	distances = RemoveSpaces(distances)
-	times = RemoveSpaces(times)
+
 	for index, timeValue := range times {
 		timeToDistance[common.StringToInt(timeValue)] = common.StringToInt(distances[index])
 	}
@@ -23,6 +19,32 @@ func ReadFile() map [int]int {
 	fmt.Printf("times to distances %v", timeToDistance)
 	return timeToDistance
 }
+
+func ReadFileLongValues() (int, int) {
+	times, distances := ReadAndBreakUpIntoComponents() 
+	timeString := ""
+	distanceString :=""
+	for index, timeValue := range times {
+		timeString = timeString + timeValue
+		distanceString = distanceString + distances[index]
+	}
+	
+	time := common.StringToInt(timeString)
+	distance := common.StringToInt(distanceString)
+	return time, distance
+}
+
+func ReadAndBreakUpIntoComponents() ( []string, []string ) {
+	file := common.ReadFileFromArgs()
+	time := strings.Split(file[0], ":")[1]
+	distance := strings.Split(file[1], ":")[1]
+	times := strings.Split(time, "  ")
+	distances := strings.Split(distance, "  ")
+	distances = RemoveSpaces(distances)
+	times = RemoveSpaces(times)
+	return times, distances
+}
+
 
 func RemoveSpaces(in []string) (out []string) {
 	for _, item := range in {
