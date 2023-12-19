0b404a5f94b4730bbca894c9c6e8560fe9aaae94
Conor McErlean <development@cmcerlean.co.uk>
2023-12-19 15:14:59 +0000

    day 12 part 2.. Yet more help

--- cmd/day12/main.go
+++ cmd/day12/main.go
@@ -3,26 +3,39 @@ package main
 import (
 	"adventOfCode23/cmd/common"
 	"fmt"
+	"strconv"
 	"strings"
 )
 
+var cache map[CacheKey]int64
+	 
  func main() {
 	 // I needed help here as my brain is fried
+
 	file := common.ReadFileFromArgs()
-	total :=0
+	total := int64(0)
 	for _, line := range file {
+		cache = make(map[CacheKey]int64)
+
+		//if i > 5 {	break}
 		parts := strings.Split(line, " ")
 		springs := parts[0]
-		counts := getCounts(parts[1]) 
+		numValues := parts[1]
+		for i := 0; i < 4; i++ {
+			springs = springs + "?" + parts[0]
+			numValues = (numValues + "," + parts[1])
+
+		}
+		counts := getCounts(numValues) 
+		//fmt.Printf("\norig: %v : %v \nunfolded: %v : %v", parts[0], getCounts(parts[1]), springs, numValues)
 		possibilities := count(springs, counts)
 		total += possibilities
-		fmt.Printf("\nPossibilities: %v", possibilities)
+
+		fmt.Printf("\nInput: %v arrangements %v", parts[0], possibilities)
+		//fmt.Printf("\nPossibilities: %v", possibilities)
 	}
 
 	fmt.Printf("\nPossible options: %v\n", total)
-
-
-
 }
 
 func getCounts(input string) []int {
@@ -34,8 +47,9 @@ func getCounts(input string) []int {
 	return counts
 }
 
-func count(springs string, counts []int) int  {
-	result := 0
+func count(springs string, counts []int) int64  {
+	result := int64(0)
+
 	if springs == "" {
 		if len(counts) == 0 {
 			return 1
@@ -49,10 +63,20 @@ func count(springs string, counts []int) int  {
 		return 1
 	}
 
+	cacheFlattenedCount := ""
+	for _, num := range counts {
+		cacheFlattenedCount = cacheFlattenedCount + "," + strconv.Itoa(num)
+	}
+	key := CacheKey{Springs: springs, Nums: cacheFlattenedCount }
+	value, exist := cache[key]
+
+	if exist {
+		return value
+	}
+
 	if springs[0] == '.' || springs[0] == '?' {
 		result += count(springs[1:], counts)
 	}
-	// 5:47 Differs
 	if springs[0] == '#' || springs[0] =='?' {
 		if counts[0] <= len(springs) && !strings.Contains(springs[:counts[0]], ".") && ( counts[0] == len(springs) || springs[counts[0]] != '#' ){
 			next := counts[0] +1
@@ -63,5 +87,11 @@ func count(springs string, counts []int) int  {
 			}
 		} 
 	}
+	cache[key] = result
 	return result
+}
+
+type CacheKey struct {
+	Springs string
+	Nums string
 }
