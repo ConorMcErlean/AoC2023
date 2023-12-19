893317089de0886229ea56d921224ef10fa2ecea
Conor McErlean <development@cmcerlean.co.uk>
2023-12-19 17:18:39 +0000

    day 10 part 2 finally complete with guide help

--- cmd/day10/main.go
+++ cmd/day10/main.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	"adventOfCode23/cmd/day10/nests"
-	"adventOfCode23/cmd/day10/pipes"
+	. "adventOfCode23/cmd/day10/pipes"
 	"fmt"
+	"slices"
 )
 
+
+// Part 2 Solution had me so lost I had to research & find a guide...
 func main() {
-	pipeDiagram, start := pipes.ReadPipeDiagram()
-	route1, route2 := pipes.GetRoutes(pipeDiagram, start)
+	pipeDiagram, start := ReadPipeDiagram()
+	route1, route2 := GetRoutes(pipeDiagram, start)
 	FindMiddle(route1, route2)
-	pipeOutline := pipes.PrintPipes(pipeDiagram, route1)
+	pipeOutline := PrintPipes(pipeDiagram, route1)
+	outside := findEnclosed(pipeOutline)
+	total := checkRemains(pipeOutline, route1, outside )
 
-	enclosed := nests.FindEnclosed(pipeOutline)
-	fmt.Printf("\n Enclosed in above %v\n", enclosed)
-	pipes.PrintPipes(pipeDiagram, route1)
-
-
+	fmt.Printf("\nTotal Inside %v", total)
 }
 
-func FindMiddle(route1 []pipes.Location, route2[]pipes.Location) {
+func FindMiddle(route1 []Location, route2 []Location) {
 	for i :=0; i < len(route1); i++ {
 		loc1 := route1[i]
 		loc2 := route2[i]
@@ -30,4 +30,55 @@ func FindMiddle(route1 []pipes.Location, route2[]pipes.Location) {
 			}
 		}
 	}
+}
+
+func checkRemains(pipes [][]rune, wall []Location, outside []Location) int {
+	count := 0
+
+	for r, row := range pipes {
+		for c := range row {
+			here := Location{X:r, Y:c }
+			if !slices.Contains(wall, here) && !slices.Contains(outside, here) {
+				count++
+			}
+		}
+	}
+	return count
+}
+
+func findEnclosed(pipes [][]rune) []Location {
+	var outside []Location
+	for r, row := range pipes {
+		within := false
+		up := false
+
+		for c, char := range row {
+			
+			if char == '|' {
+				within = !within
+			} else if char == '-' {
+				// Nothing?
+			} else if char == 'L' || char == 'F' {
+				if char == 'L' {
+					up = true
+				}
+ 
+			} else if char == '7' || char == 'J' {
+				if !up {
+					if char != '7' {
+						within = !within
+					}
+				} else {
+					if char != 'J' {
+						within = !within
+					}
+				}
+				up = false
+			}
+			if !within {
+				outside = append(outside, Location{X: r, Y: c})
+			}
+		}
+	}
+	return outside
 }
